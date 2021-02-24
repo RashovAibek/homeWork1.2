5e9c29345a7378d515fddbdfc3bb466a19611780
RashovAibek <arashov.888@gmail.com>
2021-02-24 22:08:40 +0600

    new version

--- src/com/GeekTek/Main.java
+++ src/com/GeekTek/Main.java
@@ -1,10 +1,24 @@
 package com.GeekTek;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-        goWalk(40, -20);
-        String name;
+        int age = generateRandomAge();
+        System.out.println(goWalk(age, -20));
+        System.out.println(goWalk(age, 17));
+        System.out.println(goWalk(age, -38));
+        System.out.println(goWalk(age, 15));
+        System.out.println(goWalk(age, 30));
+
+
+    }
+
+    private static int generateRandomAge() {
+        Random random = new Random();
+        int num = random.nextInt(80)+1;
+        return num;
     }
 
     public static String goWalk(int age, int temperatura) {
@@ -12,10 +26,10 @@ public class Main {
             return "можно идти гулять";
         } else if (age < 20 && (temperatura > 0 && temperatura < 28)) {
             return "можно идти гулять";
-        } else if (age > 45) {
+        } else if (age > 45 && (temperatura>-10 && temperatura<25)) {
             return "можно идти гулять";
         }else {
-            return "net ne id gulyat";
+            return "оставайтесь дома";
         }
     }
 
